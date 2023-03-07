a9defb6d492ee0a52956ec326769fdaccdb0d706
KaiLindholm <kailindholm20022@gmail.com>
2023-03-07 00:02:10 -0600

    Added 500us - 9 second delays

--- Embedded-Lab3/main.asm
+++ Embedded-Lab3/main.asm
@@ -12,23 +12,34 @@
 .def input = r19	   ; r18 is the value from the RPG 
 .def prevInput = r21
 .def temp = r18
-.def loopCt = r23
-.def iLoopRh = r25
-.def iloopRl = r24
-.equ iVal = 39998
+.def passcode = r22	
+.def passbool = r23
+.def passCount = r24
+.def mask = r25
 .cseg 
 .org 0x00
 ; Set SER (PB2), RCLK (PB3), and SRCLK (PB4) as outputs in PORTB
-clr r16
-ldi r16, (1<<DDB2) | (1<<DDB3) | (1<<DDB4)
-out DDRB, r16
+; pin A - PB0 pin B - PB1 
+; Pushbutton - PB5
+clr temp
+ldi temp, (1<<DDB2) | (1<<DDB3) | (1<<DDB4)
+out DDRB, temp
 nop
-ldi		r16, LOW(RAMEND)
-out		SPL, r16
-ldi		r16, HIGH(RAMEND)
-out		SPH, r16
+ldi		temp, LOW(RAMEND)
+out		SPL, temp
+ldi		temp, HIGH(RAMEND)
+out		SPH, temp
+
 reset: 
-	clr		value				; the digit to display first is 0
+	clr		temp
+	ldi		temp, (1<<CS01) | (1<<CS00)
+	out		TCCR0B, temp	; Timer clock = system clock / 64
+	ldi		temp, 1 << TOV0
+	out		TIFR0, temp		; CLear TOV0 / clear pending interrupts 
+	ldi		temp, 1 << TOIE0
+	sts		TIMSK0, temp	;Enable Timer/Counter0 Overflow interrrupt
+
+	ldi		value, 0x00			; the digit to display first is 0
 	clr		sequence
 
 	in		prevInput, PINB			
@@ -36,22 +47,27 @@ reset:
 
 	ldi		r30, low(digits<<1)
 	ldi		r31, high(digits<<1)
-	lpm		sequence, Z+	
+	lpm		sequence, Z			; load 0 into sequence 
+
 main:
 	rcall	display				; display a 0 to the 7-seg
 	in		input, PINB			; load in current state of RPG
+
 	andi	input, 0x03			; extract new input values 
-	ldi		loopCt, 5
-	rcall	delay10ms
 	cp		prevInput, input	 
-	brne	update_count		; if the current current state is not the same, determine the direction
+	brne	direction		; if the current current state is not the same, determine the direction
 
 	rjmp	main				
 
-update_count: 
+direction: 
 	in		input, PINB			; load in the current state again 
 	andi	input, 0x03			; extract input pins  
+
+	cp		input, prevInput	; check if they still are different 
+	breq	main
+
 	mov		temp, input			; store input in temp register
+	push	input
 
 	andi	input, 0x02			; extract B bit from input			
 	lsr		input				; shift B bit to LSB
@@ -62,13 +78,15 @@ update_count:
 	breq	decrement			; if the A and B bit are equal RPG is moving CCW
 
 	cpi		value, 0x10			; if the value is 16, we have reached the max value that can be displayed do nothing
-	brge	main					
+	breq	main					
 
 	inc		value 
-	lpm		sequence, Z+ 
+	lpm		sequence, Z			
+	adiw	Z, 1			; increment Z pointer 
 
-	mov		prevInput, temp		; store current input in prev input for next iteration 
-	rjmp	main
+	pop 	prevInput			; store current input in prev input for next iteration 
+
+	jmp	main
 
 decrement:
 	cpi		value, 0x00
@@ -78,8 +96,83 @@ decrement:
 	sbiw	Z, 0x01				; decrement Z pointer by 1  
 	lpm		sequence, Z
 
-	mov		prevInput, temp		; store current input in prev input for next iteration 
+	pop		prevInput			; store current input in prev input for next iteration 
 	rjmp	main
+
+check_pass:						; only called once the pb has been pressed for less than a second 
+	inc passCount				; increment the number of digits inputted 
+	cpi passCount, 0x05			
+	brbc 1, final
+	cp passcode, sequence 
+	breq equal
+	equal: 
+		ldi mask, 0x01			; load the mask with 1
+		ori passbool, 0x01		; load 1 into LSB 
+		lsl passbool			; shift LSB 
+	nonequal:
+		lsl passbool			; shift a 0 into the LSB 
+	final: 
+		cpi passCount, 0x1F		; if the passCOunt has a value of 0b00011111 that means each sequence matched thus password is correct
+		breq correct_password 
+		rjmp incorrect_password 
+
+correct_password: 
+	
+	rjmp reset
+
+incorrect_password: 
+
+	rjmp reset
+; prescalar: 64 start value: 131
+_delay500us: 
+	ldi		temp, 131
+	out		TCNT0, temp
+
+	wait: 
+		in		temp, TCNT0
+		cpi		temp, 0x00	; has the overflow bit been set
+		brne	wait
+
+	ret 
+		 
+_delay10ms:
+	clr		temp 
+	wait10ms: 
+		rcall	_delay500us 
+		inc		temp
+		cpi		temp, 20
+		brne	wait10ms
+	ret 
+		
+_delay1second: 
+	clr temp
+	wait1sec: 
+		rcall _delay10ms 
+		inc temp
+		cpi temp, 100
+		brne wait1sec
+	ret
+_delay2second: 
+	rcall _delay1second
+	rcall _delay1second 
+	ret 
+
+_delay5second: 
+	clr temp 
+	wait5sec: 
+		rcall _delay1second
+		inc temp
+		cpi temp, 5 
+		brne wait5sec
+	ret
+_delay9second: 
+	clr temp 
+	wait9sec: 
+		rcall _delay1second
+		inc temp
+		cpi temp, 9 
+		brne wait5sec
+	ret
 
 display:
 	push sequence			; Backup used registers on stack
@@ -111,6 +204,7 @@ end:
 	; pulse rclk, which sends values to storage register for displaying
 	sbi PORTB, PB3 ; set PB1 (RCLK)
 	nop
+
 	cbi PORTB, PB3 ; clear PB1 (RCLK)
 	nop
 	; Restore registers from stack
@@ -121,20 +215,11 @@ end:
 	cbi PORTB, PB2 ; clear pb0, ser
 	nop
 	ret
-delay10ms: 
-	ldi		iLoopRl, LOW(iVal)
-	ldi		iLoopRh, HIGH(iVal)
-iLoop: 
-	sbiw	iLoopRl, 1 
-	brne	iLoop
 
-	dec		loopCt
-	brne	delay10ms
-
-	nop
-
-	ret
 digits: 
 	.db 0x3F, 0x06, 0x5B, 0x4F, 0x66, \
 	0x6D, 0x7D, 0x07, 0x7F, 0x6F, \
 	0x77, 0xFC, 0x39, 0x5E, 0x79, 0x79, 0x71, 0
+;55969
+pass: 
+	.db 0x6D,0x6D,0x6F,0x7D,0x6F,0
