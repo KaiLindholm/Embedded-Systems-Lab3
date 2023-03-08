22ff6319f43711aa2bfcc1661ff5970c89150555
KaiLindholm <kailindholm20022@gmail.com>
2023-03-08 14:42:47 -0600

    Updated comments

--- Embedded-Lab3/main.asm
+++ Embedded-Lab3/main.asm
@@ -15,18 +15,19 @@
 .equ thirdcode = 9
 .equ fourthcode = 6
 .equ fifthcode = 9
-.def value = r20       ; r20 is the value of the display 0-0, A-F
-.def sequence = r16    ; r16 is the value of the shift register to display a value
-.def input = r19	   ; r18 is the value from the RPG 
-.def prevInput = r21
-.def temp = r18
-.def passcode = r22	
-.def passbool = r23
-.def passIndex = r24
-.def button = r25
+
+.def value = r20		; A current number in the shift register
+.def sequence = r16		; value of a sequence to be sent to Shift Register
+.def input = r18		; r18 is the value from the RPG 
+.def prevInput = r21	
+.def temp = r18			
+.def passDigit = r22	; the digit at passIndex
+.def passbool = r23		; the number of correct indices in the password 
+.def passIndex = r24	; the index the password is at in the program. 
 
 .cseg 
 .org	0x00
+
 ; Set SER (PB2), RCLK (PB3), and SRCLK (PB4) as outputs in PORTB
 ; pin A - PB0 pin B - PB1 
 ; Pushbutton - PB5
@@ -44,6 +45,7 @@ reset:
 	clr		sequence
 	clr		passbool
 	ldi		passIndex, 1 
+
 	ldi		value, 0x00			; the digit to display first is 0
 	ldi		temp, (1<<CS01) | (1<<CS00)
 	out		TCCR0B, temp	; Timer clock = system clock / 64
@@ -61,7 +63,6 @@ reset:
 	in		prevInput, PINB
 	andi	prevInput, 0x01		; extract previous A bit
 main:	
-
 	in		input, PINB
 	rcall	_delay10ms
 	sbrs	input, PB5			; check the PB state	
@@ -72,45 +73,46 @@ main:
 	cp		prevInput, input	 
 	brne	direction			; if the current current state is not the same, determine the direction
 rtn: 
-	rcall	display
-	mov		prevInput, input
-	in		input, pinb
-	andi `	input, 0x03
-	cpi		input, 0x03
-	brne	rtn
+	mov		prevInput, input	; store current value of input, in previnput for next iteration 
+	in		input, pinb			
+	andi 	input, 0x03	
+	cpi		input, 0x03			; check if the RPG is back at a non moving state
+	brne	rtn					; if not, stay in rtn until true 
 	rjmp	main		
 			
 buttonPressed:
-	rcall _delay10ms
-	checkReset: 
-		in input, PINB
-		sbrs input, PB5
-		rjmp resetCounter	
+	rcall _delay10ms		; 10ms button debounce
+	checkReset: 			; checks if the button is still held down after 10ms 
+		in input, PINB		
+		sbrs input, PB5		
+		rjmp resetCounter	; if so head to the reset counter to see if the button is held down for at least 2 seconds
 	clr r28
 	sbrc input, PB5
 	rjmp check_pass
 	rjmp main
 
-resetchecksum:
-	in input, PINB
-	sbrc input, PB5
-	rjmp reset
-	rjmp resetchecksum
-
 resetCounter: 
-	rcall _delay10ms
+	rcall _delay10ms			
 	inc r28
-	cpi R28, 200
-	breq resetchecksum
-	rjmp checkReset
+	cpi R28, 200			; determines if the button has been held down for exactly 2 seconds. 
+	breq resetchecksum		; once the counter reaches 2 seconds of delay, go to resetchecksum
+	rjmp checkReset				
+
+// only boots out of the subroutine once the button has been depressed. 
+// user is locked into reset at this point
+resetchecksum:		
+	in input, PINB
+	sbrc input, PB5			; if pin has been depressed. IE pin high, go to reset subroutine
+	rjmp reset
+	rjmp resetchecksum		; else stay in checksum 
 
 direction: 
 	in		temp, PINB
 	andi	temp, 0x02
 	lsr		temp
-	cp		input, temp
-	breq	increment 
-	rjmp	decrement 
+	cp		input, temp		; compares previous pin A state to current pin B state. 
+	breq	increment 		; if the states are equal, RPG is moving CW; Increment
+	rjmp	decrement 		; else states are not equal, RPG is moving CCW; Decrement
 
 increment: 
 	cpi		value, 0x10			; if the value is 16, we have reached the max value that can be displayed do nothing
@@ -119,48 +121,44 @@ increment:
 	inc		value
 	adiw	Z, 1
 	lpm		sequence, Z 
+
 	rcall   display
+
 	mov		prevInput, temp		; store current input in prev input for next iteration 
 	rjmp	rtn
 
 decrement:
 	cpi		value, 0x00
-	breq	finaldec
-	dec		value				; else decrement the value
-	sbiw	Z, 0x01				; decrement Z pointer by 1  
-	lpm		sequence, Z
+	breq	rtn					; if value has reached 0, do not allow shift reg to change states
+	; ----------------
+	dec		value				; if value is not zero, decrement value in register
+	sbiw	Z, 0x01				 
+	lpm		sequence, Z			; and decrement the location in program memory to the sequence of the SR
+	
 	rcall	display
+
 	mov		prevInput, temp		; store current input in prev input for next iteration 
 	rjmp	rtn
-finaldec:
-	rjmp rtn
 
 check_pass:						; only called once the pb has been pressed for less than a second 				
-;--------------
-	rcall getCode
+	rcall getPassDigit			
 	inc passIndex				; increment the number of digits inputted 
-	cpi passIndex, 0x06			
-	breq final					; branch to final if the passcount is equal to 
+	cpi passIndex, 0x06				
+	breq final					; the user and inputted 5 digits, goto final to determine validity 
 
-	cp passcode, value 
+	cp passDigit, value 		; if the current digit of the password is equal to the value on the display 
 	breq equal
-	nonequal: 
+	nonequal: 					; do nothing 
 		rjmp main
-	equal: 
-		mov temp, sequence 
-		ldi sequence, dp
-		rcall display
-		mov sequence, temp
-		rcall _delay1second
-		rcall display
+	equal: 						; increment number of correct digits
 		inc passbool
 		rjmp main	
-	final: 
-		cpi passbool, 4		; if the passCount has a value of 0b0011 1110 that means each sequence matched thus password is correct
-		breq correct_password 
-		rjmp incorrect_password 
+	final: 						; check if the number of correct digits is equal to the length of the passcode
+		cpi passbool, 4			
+		breq correct_password 	; if equal the user entered the correct password 
+		rjmp incorrect_password ; else incorrect password 
 
-getCode: 
+getPassDigit: 					; maps the current passIndex to the corresponding value of the passcode
 	cpi passIndex, 1
 	breq first
 
@@ -175,49 +173,54 @@ getCode:
 	
 	cpi passIndex, 5
 	breq fifth
- 
+
+// loads the passcode digit into pass digit for comparison 
 first: 
-	ldi passcode, firstcode
+	ldi passDigit, firstcode
 	ret 
 second: 
-	ldi passcode, secondcode
+	ldi passDigit, secondcode
 	ret 
 third: 
-	ldi passcode, thirdcode
+	ldi passDigit, thirdcode
 	ret 
 fourth: 
-	ldi passcode, fourthcode
+	ldi passDigit, fourthcode
 	ret 
 fifth: 
-	ldi passcode, fifthcode
+	ldi passDigit, fifthcode
 	ret 
 
+correct_password:		; turn on the decimal point for 5 seconds 
+	cbi PORTB, PB5		; toggless PB5, which is light L on the arduino 
+	rcall _delay10ms	
 
-correct_password:
-	cbi PORTB, PB5 
-	rcall _delay10ms
-	ldi sequence, dp
+	ldi sequence, dp	
 	rcall display
 	sbi PORTB, PB5
-	rcall _delay5second
-	rjmp reset
+	rcall _delay5second	
 
-incorrect_password: 
+	rjmp reset			
+
+incorrect_password: 	; display an underscore for 9 seconds, then reset. 
 	ldi sequence, underscore
 	rcall display
 	rcall _delay9second
+
 	rjmp reset
 
 ; prescalar: 64 start value: 6
+; temp is used through the delay subroutines. pushing temp to the stack, stores the previous calls 
+; value of temp. Used to keep the number of registers used in these subroutines to 1. 
 _delay1ms: 
-	push	temp
+	push	temp				
 	ldi		temp, 6
 	out		TCNT0, temp
 	clr		temp
 	wait: 
 		in		temp, TCNT0
-		cpi		temp, 0x00	; has the overflow bit been set
-		brne	wait
+		cpi		temp, 0x00		; has the overflow bit been set
+		brne	wait			; if not wait. 
 
 	pop		temp
 	ret 
@@ -315,10 +318,8 @@ end:
 	nop
 	ret
 
+; digits from 0-f 
 digits: 
 	.db 0x3F, 0x06, 0x5B, 0x4F, 0x66, \
 	0x6D, 0x7D, 0x07, 0x7F, 0x6F, \
 	0x77, 0x7C, 0x39, 0x5E, 0x79, 0x79, 0x71, 0
-;55969
-pass: 
-	.db 5,5,9,6,9,0
